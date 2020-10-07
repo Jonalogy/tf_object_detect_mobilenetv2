84626534eac1eaf69ac4d747727f26a9941dcd42
Jonathan Tan <jontan@palo-it.com>
2020-10-07 20:50:24 +0800

    Update model download script

--- download_mobilenet_v2.sh
+++ download_mobilenet_v2.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+WORK_DIR=$1
+MODEL_PATH=$WORK_DIR/model
 MODEL_ZIP=ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz
 
-curl http://download.tensorflow.org/models/object_detection/tf2/20200711/$MODEL_ZIP \
---output ./models/$MODEL_ZIP
+if [ ! -d $MODEL_PATH ]; then
+mkdir $MODEL_PATH
+fi
 
-tar -xzvf models/$MODEL_ZIP --directory models
-rm models/$MODEL_ZIP
+curl http://download.tensorflow.org/models/object_detection/tf2/20200711/$MODEL_ZIP \
+--output $MODEL_PATH/$MODEL_ZIP
+
+tar -xzvf $MODEL_PATH/$MODEL_ZIP --directory $MODEL_PATH
+rm $MODEL_PATH/$MODEL_ZIP
