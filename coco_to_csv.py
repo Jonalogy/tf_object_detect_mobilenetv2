e52393654553e36dacd9038b6cbbff586de20c86
Jonathan Tan <jontan@palo-it.com>
2020-10-07 18:59:42 +0800

    Update coco_to_csv.py - Introduce flag for test to train split

--- coco_to_csv.py
+++ coco_to_csv.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+import math
 from pycocotools.coco import COCO
 
 
@@ -36,10 +37,28 @@ def coco_to_csv(json_path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate csv file from xml annotations')
-    parser.add_argument('--annot_dir', help='directory of input COCO JSON files', default='../data/coco_annotations.json')
+    parser.add_argument('--annot_dir', help='directory of input COCO JSON files',
+                        default='../data/coco_annotations.json')
     parser.add_argument('--out_csv_path', help='path to output csv file', default='../data/train.csv')
+    parser.add_argument('--test_size', help="enter '0.2' for a 80% train and 20% test split", default=0)
     args = parser.parse_args()
 
     coco_df = coco_to_csv(args.annot_dir)
-    coco_df.to_csv(args.out_csv_path, index=False)
+
+    test_size_ratio = float(args.test_size)
+    print("Total records found:", len(coco_df.index))
+
+    if test_size_ratio > 0:
+        test_amt = round(test_size_ratio, 2)
+        train_amt = round(1 - test_size_ratio, 2)
+        print(f'train to test split ratio at {(train_amt, test_amt)}')
+        train_till = math.floor(len(coco_df.index) * train_amt)
+        train_df = coco_df.iloc[:train_till]
+        test_df = coco_df.iloc[train_till:]
+        print("train_size:", len(train_df.index), ", test_size:", len(test_df.index))
+        train_df.to_csv('train.csv', index=None)
+        test_df.to_csv('test.csv', index=None)
+    else:
+        coco_df.to_csv(args.out_csv_path, index=False)
+
     print('Successfully converted COCO JSON to csv.')
